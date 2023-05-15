d94b0226670174ee457d62b7e22ca74b570229e2
Roman Razvan <roman_razvan03@yahoo.com>
2023-05-15 22:15:47 +0300

    Add files via upload

---
+++ MemoryGame/MemoryGame/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace MemoryGame
+{
+    partial class MemoryGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMemoryGame = new System.Windows.Forms.Label();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.difficultyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.normalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtBoxTries = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtBoxTime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxScore = new System.Windows.Forms.TextBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pnlGame = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.pnlGame.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lblMemoryGame);
+            this.panel1.Controls.Add(this.menuStrip);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1089, 65);
+            this.panel1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(821, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Hard: 40 s, 16 tries";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(821, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Normal: 50 s, 20 tries";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(821, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Easy: 60 s, unlimited tries";
+            // 
+            // lblMemoryGame
+            // 
+            this.lblMemoryGame.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblMemoryGame.AutoSize = true;
+            this.lblMemoryGame.Font = new System.Drawing.Font("Chiller", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMemoryGame.ForeColor = System.Drawing.Color.White;
+            this.lblMemoryGame.Location = new System.Drawing.Point(423, 9);
+            this.lblMemoryGame.Name = "lblMemoryGame";
+            this.lblMemoryGame.Size = new System.Drawing.Size(230, 56);
+            this.lblMemoryGame.TabIndex = 0;
+            this.lblMemoryGame.Text = "Memory Game";
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.menuStrip.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.difficultyToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(107, 30);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // difficultyToolStripMenuItem
+            // 
+            this.difficultyToolStripMenuItem.BackColor = System.Drawing.Color.LimeGreen;
+            this.difficultyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easyToolStripMenuItem,
+            this.normalToolStripMenuItem,
+            this.hardToolStripMenuItem});
+            this.difficultyToolStripMenuItem.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.difficultyToolStripMenuItem.Name = "difficultyToolStripMenuItem";
+            this.difficultyToolStripMenuItem.Size = new System.Drawing.Size(99, 26);
+            this.difficultyToolStripMenuItem.Text = "Difficulty";
+            // 
+            // easyToolStripMenuItem
+            // 
+            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.easyToolStripMenuItem.Text = "Easy";
+            // 
+            // normalToolStripMenuItem
+            // 
+            this.normalToolStripMenuItem.Name = "normalToolStripMenuItem";
+            this.normalToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.normalToolStripMenuItem.Text = "Normal";
+            // 
+            // hardToolStripMenuItem
+            // 
+            this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
+            this.hardToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.hardToolStripMenuItem.Text = "Hard";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel2.Controls.Add(this.txtBoxTries);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.btnStart);
+            this.panel2.Controls.Add(this.txtBoxTime);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtBoxScore);
+            this.panel2.Controls.Add(this.lblScore);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 627);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1089, 75);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtBoxTries
+            // 
+            this.txtBoxTries.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.txtBoxTries.Location = new System.Drawing.Point(388, 42);
+            this.txtBoxTries.Name = "txtBoxTries";
+            this.txtBoxTries.ReadOnly = true;
+            this.txtBoxTries.Size = new System.Drawing.Size(51, 27);
+            this.txtBoxTries.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(383, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tries";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnStart.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStart.Location = new System.Drawing.Point(899, 9);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(129, 54);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            // 
+            // txtBoxTime
+            // 
+            this.txtBoxTime.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.txtBoxTime.Location = new System.Drawing.Point(657, 42);
+            this.txtBoxTime.Name = "txtBoxTime";
+            this.txtBoxTime.ReadOnly = true;
+            this.txtBoxTime.Size = new System.Drawing.Size(51, 27);
+            this.txtBoxTime.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(597, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Elapsed Time";
+            // 
+            // txtBoxScore
+            // 
+            this.txtBoxScore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtBoxScore.Location = new System.Drawing.Point(117, 42);
+            this.txtBoxScore.Name = "txtBoxScore";
+            this.txtBoxScore.ReadOnly = true;
+            this.txtBoxScore.Size = new System.Drawing.Size(51, 27);
+            this.txtBoxScore.TabIndex = 1;
+            // 
+            // lblScore
+            // 
+            this.lblScore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblScore.ForeColor = System.Drawing.Color.White;
+            this.lblScore.Location = new System.Drawing.Point(102, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(80, 30);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 65);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(15, 562);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(1074, 65);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(15, 562);
+            this.panel4.TabIndex = 3;
+            // 
+            // pnlGame
+            // 
+            this.pnlGame.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.pnlGame.Controls.Add(this.tableLayoutPanel1);
+            this.pnlGame.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlGame.Location = new System.Drawing.Point(15, 65);
+            this.pnlGame.Name = "pnlGame";
+            this.pnlGame.Size = new System.Drawing.Size(1059, 562);
+            this.pnlGame.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1059, 562);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            // 
+            // MemoryGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 702);
+            this.Controls.Add(this.pnlGame);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "MemoryGame";
+            this.Text = "MemoryGame";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.pnlGame.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblMemoryGame;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtBoxTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxScore;
+        private System.Windows.Forms.Panel pnlGame;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox txtBoxTries;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem difficultyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem normalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hardToolStripMenuItem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MemoryGame/MemoryGame/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace MemoryGame
+{
+    public partial class MemoryGame : Form
+    {
+        private int score = 0;
+        private int tries = 0;
+        private int remainingTime = 60;
+        private PictureBox[] pictureBoxes = new PictureBox[16];
+        private PictureBox firstPictureBox = null;
+        private Random random = new Random();
+        private int maxTries;
+
+        public MemoryGame()
+        {
+            InitializeComponent();
+            // Configurarea picture box-urilor
+
+            for (int i = 0; i < 16; i++)
+            {
+                pictureBoxes[i] = new PictureBox();
+                pictureBoxes[i].Size = new Size(258, 134);
+                pictureBoxes[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBoxes[i].Enabled = true;
+                pictureBoxes[i].BringToFront();
+                pictureBoxes[i].Click += PictureBox_Click;
+                tableLayoutPanel1.Controls.Add(pictureBoxes[i]);
+
+                easyToolStripMenuItem.Click += (sender, e) => SetDifficulty(60, int.MaxValue);
+                normalToolStripMenuItem.Click += (sender, e) => SetDifficulty(60, 20);
+                hardToolStripMenuItem.Click += (sender, e) => SetDifficulty(40, 16);
+
+                SetDifficulty(60, int.MaxValue); // Default difficulty: Easy
+            }
+            btnStart.Click += (sender, e) =>
+            {
+                StartGame();
+            };
+
+            // Configurarea timer-ului
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void StartGame()
+        {
+            // Inițializarea jocului
+            score = 0;
+            tries = 0;
+            txtBoxScore.Text = "0";
+            txtBoxTime.Text = "0";
+            firstPictureBox = null;
+            timer.Start();
+
+            // Configurarea imaginilor pentru picture box-uri
+            List<int> numbers = Enumerable.Range(0, 8).ToList();
+            numbers.AddRange(numbers);
+            numbers = numbers.OrderBy(x => random.Next()).ToList();
+            for (int i = 0; i < 16; i++)
+            {
+                pictureBoxes[i].Tag = new KeyValuePair<int, int>(numbers[i], -1); //-1 e tag-ul pictureBox.image
+                pictureBoxes[i].Image = Properties.Resources.question_mark;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Actualizarea timpului rămas
+            remainingTime--;
+            txtBoxTime.Text = remainingTime.ToString();
+            if (remainingTime == 0)
+            {
+                timer.Stop();
+                MessageBox.Show("Jocul s-a încheiat! Scorul tău este: " + score.ToString() + " cu numarul de incercari: " + tries.ToString());
+            }
+        }
+
+        private async void PictureBox_Click(object sender, EventArgs e)
+        {
+            // Verificarea dacă două picture box-uri sunt potrivite
+            PictureBox pictureBox = sender as PictureBox;
+            KeyValuePair<int, int> tagValuePair = (KeyValuePair<int, int>)pictureBox.Tag;
+            int number = tagValuePair.Key;
+            int imageState = tagValuePair.Value;
+            Debug.WriteLine("Click pe picture box cu tag-ul: " + pictureBox.Tag.ToString());
+            //pictureBox.Image == Properties.Resources.question_mark -aceasta conditie nu functioneaza in if, de aceea am folosit KeyValuePair
+            if (imageState == -1) //-1 tag-ul question_mark.jpg
+                {
+                if (firstPictureBox == null)
+                    {
+                        pictureBox.Image = GetImage(number);
+                        pictureBox.Tag = new KeyValuePair<int, int>(number, number);
+                        firstPictureBox = pictureBox;
+                }
+                    else
+                    {
+                        if (firstPictureBox != pictureBox)
+                        {
+                            maxTries--;
+                            if (maxTries < 0)
+                            {
+                                 timer.Stop();
+                                MessageBox.Show("Ai depășit numărul maxim de încercări! Jocul s-a încheiat.");
+                                return;
+                            }
+                        pictureBox.Image = GetImage(number);
+                            pictureBox.Tag = new KeyValuePair<int, int>(number, number);
+                            
+                        KeyValuePair<int, int> firstPictureBoxTag = (KeyValuePair<int, int>)firstPictureBox.Tag;
+                        KeyValuePair<int, int> pictureBoxTag = (KeyValuePair<int, int>)pictureBox.Tag;
+                        if (firstPictureBoxTag.Key == pictureBoxTag.Key)
+                            {
+                                score++;
+                                txtBoxScore.Text = score.ToString();
+                                firstPictureBox.Enabled = false;
+                                pictureBox.Enabled = false;
+                            if (IsSolved())
+                            {
+                                timer.Stop();
+                                MessageBox.Show("Felicitări! Ai câștigat jocul cu un scor de: " + score.ToString() + " si un numar de incercari: " + tries.ToString());
+                            }
+                        }
+                            else
+                            {
+                            await Task.Delay(1000);
+                            firstPictureBox.Image = Properties.Resources.question_mark;
+                            pictureBox.Image = Properties.Resources.question_mark;
+                      
+                            firstPictureBox.Tag = new KeyValuePair<int, int>(firstPictureBoxTag.Key, -1);
+                            pictureBox.Tag = new KeyValuePair<int, int>(pictureBoxTag.Key, -1);
+                            tries++;
+                            txtBoxTries.Text = tries.ToString();
+                        }
+                            firstPictureBox = null;
+                        }
+                    }
+
+            }
+        }
+
+        private Image GetImage(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    return Properties.Resources.a;
+                case 1:
+                    return Properties.Resources.b;
+                case 2:
+                    return Properties.Resources.c;
+                case 3:
+                    return Properties.Resources.d;
+                case 4:
+                    return Properties.Resources.e;
+                case 5:
+                    return Properties.Resources.f;
+                case 6:
+                    return Properties.Resources.g;
+                case 7:
+                    return Properties.Resources.h;
+                default:
+                    return null;
+            }
+        }
+        private bool IsSolved()
+        {
+            return pictureBoxes.All(pictureBox => !pictureBox.Enabled);
+        }
+        private void SetDifficulty(int time, int tries)
+        {
+            remainingTime = time;
+            maxTries = tries;
+        }
+
+    }
+}
